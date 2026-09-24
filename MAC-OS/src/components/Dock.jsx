4a98183b6c44d8095f0dc7e366deb562197f637a
Swarup Das <swarupdas2046@gmail.com>
2026-09-24 21:58:26 +0530

    style: update Dock component layout and icon styles

--- MAC-OS/src/components/Dock.jsx
+++ MAC-OS/src/components/Dock.jsx
@@ -5,12 +5,12 @@ const Dock = () => {
       
     <div className="icon github"><img src="\doc-icons\github.svg" alt="github" /></div>
     <div className="icon calender"><img src="\doc-icons\calender.svg" alt="calender" /></div>
-    <div className="icon link"><img src="\doc-icons\link.svg" alt="link" /></div>
-    <div className="icon mail"><img src="\doc-icons\mail.svg" alt="mail" /></div>
     <div className="icon note"><img src="\doc-icons\note.svg" alt="note" /></div>
+    <div className="icon mail"><img src="\doc-icons\mail.svg" alt="mail" /></div>
     <div className="icon pdf"><img src="\doc-icons\pdf.svg" alt="pdf" /></div>
     <div className="icon spotify"><img src="\doc-icons\spotify.svg" alt="spotify" /></div>
-
+    <div className="icon link"><img src="\doc-icons\link.svg" alt="link" /></div>
+    <div className="icon cli"><img src="\doc-icons\cli.svg" alt="cli" /></div>
     </footer>
   )
 }
