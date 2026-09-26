8fb7f904622a238ac25a21ca4e3ae623c9ebaa6d
Swarup Das <swarupdas2046@gmail.com>
2026-09-26 11:54:43 +0530

    feat: add new Note window with markdown support and syntax highlighting
    - Introduced a new Note component that fetches and displays markdown content from note.txt.
    - Integrated react-markdown and react-syntax-highlighter for rendering markdown and code syntax.
    - Updated App component to include the new Note window.
    - Enhanced styling for the Note window and adjusted existing styles for better UI.
    - Added new dependencies: react-markdown and react-syntax-highlighter.
    - Included a new PDF resume for Swarup Das in the public directory.
    - Created a detailed note.txt file outlining developer journey, projects, and skills.

--- check/src/App.jsx
+++ check/src/App.jsx
@@ -1,14 +1,16 @@
 import "./app.scss";
 import Dock from "./components/Dock";
 import Nav from "./components/Nav";
-import Github from "./windows/github";
+import Github from "./windows/Github";
 import MacWindow from "./windows/MacWindow";
+import Note from "./windows/Note-Window/Note";
 const App = () => {
   return (
     <main>
       <Nav />
       <Dock />
       <Github />
+      <Note />
     </main>
   );
 };
---
+++ check/src/windows/Note-Window/Note.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import MacWindow from '../MacWindow'
+import Markdown from 'react-markdown'
+import './Note.scss'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atelierCaveDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+const Note = () => {
+
+    const [markDown, setMarkDown] = useState(null)
+
+useEffect(()=>{
+    fetch("/note.txt").then(res=>res.text()).then(data=>setMarkDown(data))
+},[])
+
+
+  return (
+
+    <MacWindow>
+    <div className="note-content">
+        {markDown && <SyntaxHighlighter language="typescript" style={atelierCaveDark}>{markDown}</SyntaxHighlighter>}
+    </div>
+    </MacWindow>
+
+)
+}
+
+export default Note
